06612f576274e893cb80ddb0e5da679a69015781
Grant Shoshin Shangreaux <shshoshin@protonmail.com>
2019-09-03 00:00:22 -0500

    Docs: comment on return val for matrix:login_user

--- src/matrix.erl
+++ src/matrix.erl
@@ -1,6 +1,8 @@
 -module(matrix).
 -export([login_user/3]).
 
+%% returns a http response with a json body that
+%% includes the access token and other user details
 login_user(Homeserver, User, Pwd) ->
     URL = Homeserver ++ "/_matrix/client/r0/login",
     Type = "application/json",
