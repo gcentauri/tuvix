c31756b9b83445e56715372b8b7abc1689c39c7c
Grant Shoshin Shangreaux <shshoshin@protonmail.com>
2019-09-04 22:29:45 -0500

    Add: create_room function, post helper for simple json request

--- src/matrix.erl
+++ src/matrix.erl
@@ -1,22 +1,43 @@
 -module(matrix).
 -export([login_user/3]).
+-compile(export_all).
+
+%% everything in the matrix API is json
+%% strings must be UTF-8 so make sure your
+%% keys and values are atoms or binaries
+post(Url, Map) ->
+    Body = jsone:encode(Map),
+    httpc:request(post,
+                  {Url, [], "application/json", Body},
+                  [],
+                  []).
 
 %% returns a http response with a json body that
 %% includes the access token and other user details
 login_user(Homeserver, User, Pwd) ->
-    URL = Homeserver ++ "/_matrix/client/r0/login",
-    Type = "application/json",
-    Body = jsone:encode(
-             #{
-               type => <<"m.login.password">>,
-               identifier => #{
-                               type => <<"m.id.user">>,
-                               user => User
-                              },
-               password => Pwd
-              }
-             ),
     inets:start(),
     ssl:start(),
 
-    httpc:request(post, {URL, [], Type, Body}, [], []).
+    URL = Homeserver ++ "/_matrix/client/r0/login",
+    ReqBody = #{
+                type => <<"m.login.password">>,
+                identifier => #{
+                                type => <<"m.id.user">>,
+                                user => User
+                               },
+                password => Pwd
+               },
+
+    {ok, {{Version, 200, ReasonPhrase}, Headers, RespBody}} =
+        post(URL, ReqBody),
+
+    jsone:decode(list_to_binary(RespBody)).
+
+%% Token should be a string, as it goes in a query param
+%% returns map with room_id
+create_room(Homeserver, Token) ->
+    Url = Homeserver ++ "/_matrix/client/r0/createRoom?access_token=" ++ Token,
+    {ok, {{Version, 200, ReasonPhrase}, Headers, RespBody}} =
+        post(Url, #{}),
+    jsone:decode(list_to_binary(RespBody)).
+
